d275e313607a10c24a93fb5dc4b0f363552a8c41
Wouter Klein Heerenbrink <wouter@fluxility.com>
2014-02-24 13:34:30 +0100

    Add field to example project to test invalidation

--- example_project/example_app/models.py
+++ example_project/example_app/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 
 class SomeModel(models.Model):
+    some_char_field = models.CharField(max_length=100, null=True)
     some_file_field = models.FileField(upload_to="some_folder")
