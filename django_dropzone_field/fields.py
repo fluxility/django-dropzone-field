b90110cdbee1607dd32a06e3a26d576ea5916c6e
Wouter Klein Heerenbrink <wouter@fluxility.com>
2014-02-24 14:02:47 +0100

    Show initial file on invalid form

--- django_dropzone_field/fields.py
+++ django_dropzone_field/fields.py
@@ -8,16 +8,28 @@ from django_dropzone_field.widgets import DropzoneWidget
 class DropzoneField(FileField):
     widget = DropzoneWidget
 
+    def bound_data(self, data, initial):
+        if data in self.empty_values:
+            return initial
+
+        return super(DropzoneField, self).bound_data(data, initial)
+
+
     def clean(self, data, initial=None):
         return super(DropzoneField, self).clean(data, initial)
 
     def to_python(self, value):
+        if value in self.empty_values:
+            return None
+
         try:
             upload = TemporaryUpload.objects.get(hash=value)
             file_object = File(upload.file)
             return file_object
         except TemporaryUpload.DoesNotExist:
             raise ValidationError("Selected image does not exist")
+
+
 
 
 
